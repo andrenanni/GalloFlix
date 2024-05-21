447eb19c89d202c5f8819a63b4540f4ece40d93a
andrenanni <andre-nanni@hotmail.com>
2024-05-21 20:37:03 -0300

    Continuaçâo do AccountController

--- NanniFlix/Controllers/AccountController.cs
+++ NanniFlix/Controllers/AccountController.cs
@@ -1,6 +1,8 @@
 using System.Net.Mail;
+using System.Security.Claims;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages;
 using NanniFlix.ViewModels;
 
 namespace NanniFlix.Controllers;
@@ -8,8 +10,8 @@ namespace NanniFlix.Controllers;
 public class AccountController : Controller
 {
     private readonly ILogger<AccountController> _logger;
-    private readonly object _singInManager;
-    private UserManager<IdentityUser> _userManager;
+    private readonly SignInManager<IdentityUser> _signInManager;
+    private readonly UserManager<IdentityUser> _userManager;
 
     public AccountController(
         ILogger<AccountController> logger,
@@ -18,7 +20,7 @@ public class AccountController : Controller
         )
     {
         _logger = logger;
-        _singInManager = signInManager;
+        _signInManager = signInManager;
         _userManager = userManager;
     }
 
@@ -34,11 +36,49 @@ public class AccountController : Controller
 
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Login(LoginVM login){
-        if (ModelState.IsValid){
-            
+    public async Task<IActionResult> Login(LoginVM login)
+    {
+        if (ModelState.IsValid)
+        {
+            string userName = login.Email;
+            if (IsValidEmail(userName))
+            {
+                var user = await _userManager.FindByEmailAsync(userName);
+                if (user != null)
+                {
+                    userName = user.UserName;
+                }
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(
+                userName, login.Password, login.RememberMe, lockoutOnFailure: true
+            );
+            if (result.Succeeded)
+            {
+                _logger.LogInformation($"Usúario {userName} fez login");
+                return LocalRedirect(login.ReturnUrl);
+            }
+
+            if (result.IsLockedOut)
+            {
+                _logger.LogWarning($"Usúario {userName} foi bloqueado");
+                ModelState.AddModelError(string.Empty, "Conta bloqueada! Aguarde alguns minutos para tentar novamente");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Usuáruios e/ou senha inválidos");
+            }
+
         }
         return View(login);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Logout(){
+        _logger.LogInformation($"Usúario {ClaimTypes.Email} saiu do sistema");
+        await _signInManager.SignOutAsync();
+        return RedirectToAction("Index", "Home");
     }
 
     private static bool IsValidEmail(string email)
